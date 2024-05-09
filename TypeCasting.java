f739390ba46e2d057ee1d42c5e4063c8de932636
“Yash <Yash.chauhan@infostride.com>
2024-05-09 16:14:18 +0530

    First commit

---
+++ TypeCasting.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class TypeCasting {
+    public static void main(String[] args) {
+//        Implicit Typecasting or widening: When we assign a small
+//        datatype value to a bigger datatype
+    }
+}
