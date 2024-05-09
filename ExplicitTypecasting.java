17ae9a1e20c061d7e5dd8899972ade466fdc73c4
“Yash <Yash.chauhan@infostride.com>
2024-05-09 19:43:27 +0530

    Typecasting basics

---
+++ ExplicitTypecasting.java
@@ -0,0 +1,55 @@
+//        Explicit Typecasting or narrowing: When we assign a big
+//        datatype value to a smaller datatype variable
+
+import java.util.Scanner;
+
+public class ExplicitTypecasting {
+
+    Scanner sc = new Scanner(System.in);
+
+    public void floatToInt() {
+        System.out.print("Enter float value: ");
+        float num1 = sc.nextFloat();
+        int num2 = (int) num1;
+        System.out.println("Float to Int: " + num2);
+    }
+
+    public void intToByte() {
+        System.out.print("Enter int value: ");
+        int num3 = sc.nextInt();
+        byte num4 = (byte) num3;
+        System.out.println("Int to Byte: " + num4);
+    }
+
+    public void doubleToByte() {
+        System.out.print("Enter Double value: ");
+        double num5 = sc.nextDouble();
+        byte num6 = (byte) num5;
+        System.out.println("Double to Byte: " + num6);
+    }
+
+    public void floatToChar() {
+        System.out.print("Enter float value: ");
+        float num7 = sc.nextFloat();
+        char num8 = (char) num7;
+        System.out.println("Float to Char: " + num8);
+    }
+
+    public void longToInt() {
+        System.out.print("Enter long value: ");
+        long num9 = sc.nextLong();
+        int num10 = (int) num9;
+        System.out.println("Float to Char: " + num10);
+    }
+
+    public static void main(String[] args) {
+
+        ExplicitTypecasting ob1 = new ExplicitTypecasting();
+        ob1.floatToInt();
+        ob1.intToByte();
+        ob1.doubleToByte();
+        ob1.floatToChar();
+        ob1.longToInt();
+    }
+}
+
---
+++ ImplicitTypeCasting.java
@@ -0,0 +1,59 @@
+//        Implicit Typecasting or widening: When we assign a small
+//        datatype value to a bigger datatype variable
+
+import java.util.Scanner;
+
+public class ImplicitTypeCasting {
+
+    Scanner sc = new Scanner(System.in);
+
+    public void byteToInt() {
+        System.out.print("Enter Byte value: ");
+        byte num1 = sc.nextByte();
+        int num2 = num1;
+        System.out.println("Byte to Int: " + num2);
+    }
+
+    public void shortToInt() {
+
+        System.out.print("Enter Short value: ");
+        short num3 = sc.nextShort();
+        int num4 = num3;
+        System.out.println("Short to Int: " + num4);
+    }
+
+    public void intToFloat() {
+
+        System.out.print("Enter Int value: ");
+        int num5 = sc.nextInt();
+        float num6 = num5;
+        System.out.println("Int to Float: " + num6);
+    }
+
+    public void charToInt() {
+
+        System.out.print("Enter a character value: ");
+        char num7 = sc.next().charAt(0);
+        int num8 = num7;
+        System.out.println("Char to Int: " + num8);
+    }
+
+    public void longToDouble() {
+
+        System.out.print("Enter long value: ");
+        long num9 = sc.nextByte();
+        double num10 = num9;
+        System.out.println("Long to Double: " + num9);
+    }
+
+    public static void main(String[] args) {
+
+        ImplicitTypeCasting ob1 = new ImplicitTypeCasting();
+
+        ob1.byteToInt();
+        ob1.shortToInt();
+        ob1.intToFloat();
+        ob1.charToInt();
+        ob1.longToDouble();
+    }
+}
